a423863b5dfa2b4717d87051d47959378a12b43c
Fesenko Anton <antfesenko@vodafone.ua>
2019-07-12 19:23:19 +0300

    First commit

---
+++ test/DistinctsWordsTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class DistinctsWordsTest {
+
+    @Test
+    public void test1(){
+        DistinctsWords distinctFileWords = new DistinctsWords("E:\\Projects\\test\\First\\src\\1.txt");
+        Set<String> wordList = distinctFileWords.getWordList();
+        assertEquals("[aaa, fxcc, 2, weffwefsx, fewwef, 23f, jr2l3f3f, j33, \\, fwefwef, j24, 32j3]", wordList.toString() );
+    }
+
+}
