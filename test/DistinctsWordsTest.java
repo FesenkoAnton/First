c9a3a24aa454b09d60cbc9a674559458c7e2d4d1
Fesenko Anton <antfesenko@vodafone.ua>
2019-07-15 13:07:07 +0300

    first commit

--- test/DistinctsWordsTest.java
+++ test/DistinctsWordsTest.java
@@ -11,6 +11,7 @@ public class DistinctsWordsTest {
         DistinctsWords distinctFileWords = new DistinctsWords("E:\\Projects\\test\\First\\src\\1.txt");
         Set<String> wordList = distinctFileWords.getWordList();
         assertEquals("[aaa, fxcc, 2, weffwefsx, fewwef, 23f, jr2l3f3f, j33, \\, fwefwef, j24, 32j3]", wordList.toString());
+        
     }
 
 }
